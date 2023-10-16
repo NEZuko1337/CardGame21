9e8841a961e636e0c52ae4540dbbe46f4c2b4b4f
Zuko1337 <zuko13371@yandex.ru>
2023-10-16 20:30:13 +0300

    линки к фоткам

---
+++ cardbot/links_to_photos.py
@@ -0,0 +1,21 @@
+link_to_all_cards = 'https://ibb.co/YcCqssw'
+
+link_to_2 = 'https://ibb.co/qntB9M7'
+
+link_to_3 = 'https://ibb.co/Pc4Qrw1'
+
+link_to_4 = 'https://ibb.co/ygm3GnF'
+
+link_to_5 = 'https://ibb.co/T0j2dBG'
+
+link_to_6 = 'https://ibb.co/0DMLMph'
+
+link_to_7 = 'https://ibb.co/ZJGXDs6'
+
+link_to_8 = 'https://ibb.co/9vSpjFv'
+
+link_to_9 = 'https://ibb.co/QdGV8By'
+
+link_to_10 = 'https://ibb.co/qYZSsYZ'
+
+link_to_11 = "https://ibb.co/c2pjcvj"
